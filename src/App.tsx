4e5a2a26541bd0cdf18eceb34993edf5bc7b2062
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-06 00:24:49 +0530

    feat: update import paths and structure in App component for improved organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import HeroSection from "./components/HeroSection";
+import HeroSection from "@/components/HeroSection";
+import FeaturesSection from "@/components/FeaturesSection";
+import ExclusiveSection from "./components/ExclusiveSection";
+import TeamTriaxx from "./components/TeamTriaxx";
+import InventorySection from "./components/InventorySection";
+import ExperienceSection from "./components/ExperienceSection";
+import PlanSection from "./components/PlanSection";
+import HowItWorksSection from "./components/HowItWorksSection";
 
 function App() {
   return (
     <div className="">
       <HeroSection />
+      <HowItWorksSection />
+      <FeaturesSection />
+      <ExclusiveSection />
+      <TeamTriaxx />
+      <InventorySection />
+      <ExperienceSection />
+      <PlanSection />
     </div>
   );
 }
