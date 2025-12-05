e178ccb19759d75610ab02b3cb1f9455ecb4b66e
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-05 14:19:13 +0530

    feat: add Header component with navigation and call-to-action button

---
+++ src/components/Header.tsx
@@ -0,0 +1,57 @@
+import triaxxLogo from "@/assets/triaxx-logo.svg";
+import { Button } from "./ui/button";
+import { ArrowUpRight } from "lucide-react";
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between py-6 px-5">
+      <img src={triaxxLogo} alt="Triaxx Logo" className="h-8" />
+
+      <nav aria-label="Primary" className="flex-1">
+        <ul className="hidden md:flex justify-center gap-10 text-slate-900  font-medium tracking-wide">
+          <li>
+            <a
+              href="/"
+              className="transition-colors duration-150 hover:text-sky-600"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="/subscriptions"
+              className="transition-colors duration-150 hover:text-sky-600"
+            >
+              Subscriptions
+            </a>
+          </li>
+          <li>
+            <a
+              href="/contact"
+              className="transition-colors duration-150 hover:text-sky-600"
+            >
+              Contact Us
+            </a>
+          </li>
+          <li>
+            <a
+              href="/about"
+              className="transition-colors duration-150 hover:text-sky-600"
+            >
+              About Us
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div>{/* cta placeholder (e.g., sign in / button) */}</div>
+
+      <Button variant="outline" className="rounded-full">
+        <span>Register Now</span>
+        <div className="size-6 bg- flex items-center justify-center rounded-full ">
+          <ArrowUpRight className="size-4 bg-transparent bg-primary-gradient text-clip" />
+        </div>
+      </Button>
+    </header>
+  );
+}
