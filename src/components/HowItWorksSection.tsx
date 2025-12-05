76748008db409180f27e3bfc4a5e0de885fced22
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-06 00:10:46 +0530

    feat: add How It Works section component with illustration

---
+++ src/components/HowItWorksSection.tsx
@@ -0,0 +1,13 @@
+import howItWorksIllustration from "@/assets/how-it-works.png";
+
+export default function HowItWorksSection() {
+  return (
+    <div className="bg-white py-20 max-w-6xl mx-auto">
+      <img
+        src={howItWorksIllustration}
+        alt="How It Works"
+        className="mx-auto"
+      />
+    </div>
+  );
+}
