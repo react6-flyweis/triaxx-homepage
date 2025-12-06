110dc9895edf545b7dca27f9b41b8b30893fb707
Ahmadullah Mirza <mirzaahmad9028.am@gmail.com>
2025-12-07 00:36:26 +0530

    feat: implement lazy loading for sections and wrap in Suspense for improved performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import HeroSection from "@/components/HeroSection";
-import FeaturesSection from "@/components/FeaturesSection";
-import ExclusiveSection from "./components/ExclusiveSection";
-import TeamTriaxx from "./components/TeamTriaxx";
-import InventorySection from "./components/InventorySection";
-import ExperienceSection from "./components/ExperienceSection";
-import PlanSection from "./components/PlanSection";
-import HowItWorksSection from "./components/HowItWorksSection";
+import { lazy, Suspense } from "react";
+import Loading from "./components/Loading";
+
+const HeroSection = lazy(() => import("@/components/HeroSection"));
+const FeaturesSection = lazy(() => import("@/components/FeaturesSection"));
+const ExclusiveSection = lazy(() => import("./components/ExclusiveSection"));
+const TeamTriaxx = lazy(() => import("./components/TeamTriaxx"));
+const InventorySection = lazy(() => import("./components/InventorySection"));
+const ExperienceSection = lazy(() => import("./components/ExperienceSection"));
+const PlanSection = lazy(() => import("./components/PlanSection"));
+const HowItWorksSection = lazy(() => import("./components/HowItWorksSection"));
+const Footer = lazy(() => import("./components/Footer"));
+const ConnectSection = lazy(() => import("./components/ConnectSection"));
+const Testimonial = lazy(() => import("./components/Testimonial"));
 
 function App() {
   return (
     <div className="">
-      <HeroSection />
-      <HowItWorksSection />
-      <FeaturesSection />
-      <ExclusiveSection />
-      <TeamTriaxx />
-      <InventorySection />
-      <ExperienceSection />
-      <PlanSection />
+      <Suspense fallback={<Loading />}>
+        <HeroSection />
+        <HowItWorksSection />
+        <FeaturesSection />
+        <ExclusiveSection />
+        <TeamTriaxx />
+        <InventorySection />
+        <Testimonial />
+        <ExperienceSection />
+        <PlanSection />
+        <ConnectSection />
+        <Footer />
+      </Suspense>
     </div>
   );
 }
